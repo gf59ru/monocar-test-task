ef8b94663b5409a8285b1d04ec729d3883dac52e
General Failure <gf59ru@live.com>
2020-08-05 13:06:18 +0500

    Initial Commit

---
+++ test task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test task
+//
+//  Created by Nikolai Borovennikov on 05.08.2020.
+//  Copyright © 2020 monocar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
